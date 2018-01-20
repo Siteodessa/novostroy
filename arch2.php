3a61953fbb69fa9960aa5d8c666e6b05a41491c2
Siteodessa <siteodessa.com@gmail.com>
2018-01-21 01:50:45 +0300

    custom filters bugs detected

--- arch2.php
+++ arch2.php
@@ -13,23 +13,6 @@ div#srch_stat {
     width: 100%;
     display: flex;
     justify-content: flex-end;
-}.fixie {
-    position: fixed;
-    top: 20px;
-    background: green;
-    color: #fff;
-    padding: 12px;
-    font-size: 17.5px;
-    left: 0;
-}.fixie {
-    position: fixed;
-    top: 20px;
-    background: green;
-    color: #fff;
-    padding: 12px;
-    font-size: 17.5px;
-    z-index: 999999;
-    left: 0;
 }
 div.special_counter {
     text-align: right;
@@ -71,14 +54,7 @@ b {
     padding: 5px 4px 0 0;
     border-left: 1px solid;
     margin: 0 7px 0 0px;
-} .editka a.post-edit-link { padding: 5px; display:  inline; } button#searchstarter {
-    height: 40px;
-    width: 80px;
-}.search_values {
-    display: grid;
-    grid-template-columns: 100px 350px 190px 180px 180px 80px;
-    grid-gap: 15px;
-}.search_values div { margin: 0 10px; } .search_values .sqrt_inp input, .search_values .prc_inp input { width: 70px; margin: 0 10px 0 0; } p.srch_labl { margin: 0; background: #bdbdbde6; color: #444444; font-weight: bold; padding: 4px; } aside { background-color: #6ac2e71f; }
+} .editka a.post-edit-link { padding: 5px; display:  inline; } .search_values { display: grid; grid-template-columns: 100px 350px 190px 180px 180px; grid-gap: 15px; } .search_values div { margin: 0 10px; } .search_values .sqrt_inp input, .search_values .prc_inp input { width: 70px; margin: 0 10px 0 0; } p.srch_labl { margin: 0; background: #bdbdbde6; color: #444444; font-weight: bold; padding: 4px; } aside { background-color: #6ac2e71f; }
 </style><?php
 function default_kvartiri_behaviour()
 {
@@ -88,18 +64,18 @@ function default_kvartiri_behaviour()
       echo '<div class="'. $search_tag .'_values"><aside><p class="srch_labl">'. $field['label']  .'</p>';
       foreach( $field['choices'] as $k => $v )
       {
-         echo '<div class="choice" data-filter="'. $search_tag .'"><label title="'. $v . '" class="srch_labl_val">'. $k . '</label><input type="checkbox" placeholder="' . $k . '" value="' . $v . '"><br></div>';
+         echo '<div class="choice" data-filter="'. $search_tag .'"><label class="srch_labl_val">'. $k . '</label><input type="checkbox" placeholder="' . $k . '" value="' . $v . '"><br></div>';
       };
        echo '</aside></div>';
      };
   };
-print_r('<a href="http://novostroy/kvartiri/?rom=2&bld=Кадорр&block=Киевский&mnp=10000&mxp=20000&mns=20.2&mxs=40"></a>');
+print_r('<a href="http://novostroy/kvartiri/?rom=2&bld=Кадорр&block=Киевский&mnp=10000&mxp=20000&mns=20.2&mxs=40">Следующий этап - НАЧНИ С ЭТОГО твой onchange должен начать заставить также обрабатывать и другие по отдельности, и потом уже их группами , а еще тут Проверка поиска если сюда нажать</a>');
 print_r('<div class="search_values">');
 make_search_values_checkboxes("rom");
 make_search_values_checkboxes("bld");
 make_search_values_checkboxes("block");
 print_r('<div class="sqrt_inp"><p>Площадь</p><label><input type="text" placeholder="От" /> </label><label><input type="text" placeholder="До" /></label></div>');
-print_r('<div class="prc_inp"><p>Цена</p><label><input type="text" placeholder="От" /> </label><label><input type="text" placeholder="До" /></label></div><a id="searchstarter"><button>Поиск</button></a></div>'); // end search_values block
+print_r('<div class="prc_inp"><p>Цена</p><label><input type="text" placeholder="От" /> </label><label><input type="text" placeholder="До" /></label></div></div>'); // end search_values block
  function push_all_possible_meta_values($meta_key_slug)
  { $arr =  array(); $search_tago = $meta_key_slug; $field = get_field_object($search_tago); if( $field ) { foreach( $field['choices'] as $k => $v ) { array_push($arr, $k); } } return $arr; }
  $rom_value = push_all_possible_meta_values("rom"); //default show all rooms possible
@@ -145,97 +121,60 @@ default_kvartiri_behaviour();
       get_footer();
    ?>
    <script>
- let app_res = jQuery('.appartment_res '); let special_counter = jQuery('div.special_counter'); special_counter.prependTo(app_res); special_counter.wrap('<div id="srch_stat"></div>');
-</script>
+   let app_res = jQuery('.appartment_res ');
+   let special_counter = jQuery('div.special_counter');
+special_counter.prependTo(app_res);
+special_counter.wrap('<div id="srch_stat"></div>');
+   </script>
 <script type="text/javascript">
-  function magicBox(a, b) {
-    function detectGetParameters() {
-    function getParameterByName(name, url) {
-      if (!url) url = window.location.href;
-      name = name.replace(/[\[\]]/g, "\\$&");
-      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-      if (!results) return null;
-      if (!results[2]) return '';
-      return decodeURIComponent(results[2].replace(/\+/g, " "));
-    };
-    var possible_received_args = ['rom', 'bld', 'block', 'mnp', 'mxp', 'mns', 'mxs'];
-    var pral = possible_received_args.length;
-    var search_object = {};
-    for (k = 0; k < pral; k++) {
-      if ((getParameterByName(possible_received_args[k])) != null) {
-        var received_arg_name = (possible_received_args[k]);
-        var received_args_value = (getParameterByName(possible_received_args[k]));
-        search_object[received_arg_name] = received_args_value;
-      };
-    };
-      return search_object;
-    }
-    //returns search object from get parameters
-    function set_checked_inputs() {
-jQuery.each(detectGetParameters(), function(name, value) { 
-    var arra = value.split(',');
-for (var i = 0; i < arra.length; i++) {
-  var active_div = jQuery('div.' + name + '');
-     jQuery(active_div).each( function() { 
-           var active_input = jQuery(this).find('input');
-      if (active_input.attr('value') == arra[i])
-{
-active_input.attr('checked',true);
-active_input.attr('marked',true);
-}
-   })
-}
- }); 
-    };
-    new set_checked_inputs();
-    //inputs are now marked due to user's choice, now we re ready to pick new searches 
-    (function($) {
-      ayhu.each(function(i) {
-        var df = jQuery(this).data('filter');
-        jQuery(this).attr('data-count', df + (i + 1));
-        jQuery(this).addClass(df);
-      });//adds class and numbered class to filters
-      var fdf = a.data('filter');      
-      b.each(function() {
-        $(this).on('change', 'input[type="checkbox"]', function() { 
-          var url = '<?php echo home_url('kvartiri'); ?>';
-          args = {};
-          a.each(function() {
-            var filter = $(this).data('count'),
-              vals = [];
-            jQuery(this).find('input:checked').each(function() {
-              vals.push($(this).val());
-            });
-            args[filter] = vals.join(',');
-          });                                 
-          url += '/?';  
-          url += fdf;
-          url += '=';
-          jQuery.each(args, function(name, value) { 
-            if (value != '') {
-              url += value + ',';
-            }
-          }),
-          url = url.slice(0, -1);
-          console.log(url);
-          jQuery('#searchstarter').attr('href', url);
-          jQuery('.fixie').text(url);
-          window.location.replace( url );
-        });
-      });
-    })(jQuery);
+  function magicBox(a,b){
+
+//  перед каментом update url надо вставить проверку на то , есть ли в ссылке другие гетовые запросы
+//  и если есть - не использовать url текущим - он не учитывает состояние а берет базовый. Нужно взять все геты поштучно проверить if ( 1=1||2!=3 ||bla ) затем сделать проверку текущего гет пожелания , не совпадает ли оно с текущим
+//  , если совпадает - вроде должно each обработаться, если нет - url заменяется на текущий window.location.href += '&' += fdf += '=' += value .......value уже в иче должен внедряться. При тестировании найдо проверить что будет если выбрать
+//  1 комната потом район примор потом 2 комната и посмотреть вставится ли значение комнаты в нужную часть гета, а хуй там правда что вставится
+
+  (function($) {
+      a.each(function(i){
+         var df = jQuery(this).data('filter');
+    jQuery(this).attr('data-count', df + (i+1));
+  });
+  var fdf = a.data('filter');
+	b.each(function(){
+      $(this).on('change', 'input[type="checkbox"]', function(){ // change
+		var url = '<?php echo home_url('kvartiri'); ?>';// vars
+			args = {};
+		a.each(function(){    // loop over filters
+			var filter = $(this).data('count'),
+				vals = [];
+			jQuery(this).find('input:checked')
+      .each(function()
+      { // find checked inputs
+				vals.push( $(this).val() );
+			});
+			args[ filter ] = vals.join(',');// append to args
+		});
+		url += '?';// update url
+        url += fdf;
+        url += '=';
+		jQuery.each(args, function( name, value ){// loop over args
+          if (value != ''){
+		 url += value + ',';
+          }
+		});
+		url = url.slice(0, -1);// remove last &
+
+        console.log( url );
+//		window.location.replace( url );// reload page
+	});
+    });
+})(jQuery);
   };
-  var z = jQuery('.rom_values .choice');
-  var f = jQuery('.rom_values');
-  var d = jQuery('.bld_values .choice');
-  var j = jQuery('.bld_values');
-  var n = jQuery('.block_values .choice');
-  var p = jQuery('.block_values');
-  new magicBox(z, f), 
-  new magicBox(d, j),
-  new magicBox(n, p);
-  jQuery('body').append('<div class="fixie"></div>');
-  if(window.location.href.indexOf('?') != -1 && window.location.href.indexOf('=') != -1)
-  jQuery('.fixie').text(window.location.href)
+var z = jQuery('.rom_values .choice ');
+var f = jQuery('.rom_values ');
+var d = jQuery('.bld_values .choice ');
+var j = jQuery('.bld_values ');
+var n = jQuery('.block_values .choice ');
+var p = jQuery('. block_values ');
+new magicBox(z, f), new magicBox(d, j), new magicBox(n, p);
 </script>
